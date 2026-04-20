a6ffc4f54c23f6d32567b862f30cc661e2f5a3b5
danilo-robert <99danilorobert@gmail.com>
2026-04-20 17:20:05 -0400

    implementando validações

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,11 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SiteContato;
 use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function contato(){
+    public function contato(Request $request){
+
         return view('site.contato', ['titulo' => 'Contato']);
+    }
+
+    public function salvar(Request $request){
+
+        $request->validate([
+            'nome' => 'required|min:3|max:40',
+            'telefone' => 'required',
+            'email' => 'required',
+            'motivo_contato' => 'required',
+            'mensagem' => 'required|max:2000'
+        ]);
+        SiteContato::create($request->all());
     }
 }
--- app/Models/SiteContato.php
+++ app/Models/SiteContato.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class SiteContato extends Model
 {
     use HasFactory;
+    protected $fillable = ['nome', 'email', 'telefone', 'motivo_contato', 'mensagem'];
 }
