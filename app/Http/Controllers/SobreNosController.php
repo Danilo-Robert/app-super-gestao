1b7b640459da78d1926955317b61126f565baa97
danilo-robert <99danilorobert@gmail.com>
2026-04-22 17:08:27 -0400

    implementado middleware

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 
 class SobreNosController extends Controller
 {
+   
     public function sobreNos(){
         return view('site.sobre-nos', ['titulo' => 'Sobre Nós']);
     }
