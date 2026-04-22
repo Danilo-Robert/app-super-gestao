e52ba218da9f148ff45b5cf4deef178e0053ceea
danilo-robert <99danilorobert@gmail.com>
2026-04-22 11:24:36 -0400

    persistindo contatos

--- app/Models/SiteContato.php
+++ app/Models/SiteContato.php
@@ -9,5 +9,5 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class SiteContato extends Model
 {
     use HasFactory;
-    protected $fillable = ['nome', 'email', 'telefone', 'motivo_contato', 'mensagem'];
+    protected $fillable = ['nome', 'email', 'telefone', 'motivo_contatos_id', 'mensagem'];
 }
