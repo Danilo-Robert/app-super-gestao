78e1fd4b57a8d775cffe107f57426f4bd2c41fd6
danilo-robert <99danilorobert@gmail.com>
2026-04-30 17:17:17 -0400

    implementado belongto para relacionamento hasMany

---
+++ database/migrations/2026_04_30_194228_alter_produtos_relacionamento_fornecedores.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('produtos', function (Blueprint $table) {
+
+            //inserindo um registro de fornecedor para estabelecer o relacionamento
+            $fornecedor_id = DB::table('fornecedores')->insertGetId([
+                'nome' => 'Fornecedor Padrão SG',
+                'site' => 'fornecedorpadraosg.com.br',
+                'uf' => 'SP',
+                'email' => 'fornecedorpadraosg@email.com',
+            ]);
+
+            $table->unsignedBigInteger('fornecedor_id')->default($fornecedor_id)->after('id');
+            $table->foreign('fornecedor_id')->references('id')->on('fornecedores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('produto', function(Blueprint $table){
+            $table->dropForeign('produtos_fornecedor_id_foreign');
+            $table->dropColumn('fornecedor_id');
+        }); 
+    }
+};
