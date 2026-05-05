919f5de6108b839c63162b5b4cc1b92defd896d2
danilo-robert <99danilorobert@gmail.com>
2026-05-05 16:13:11 -0400

    implementado relação de pedido ao produto

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -16,4 +16,13 @@ class Item extends Model
     public function fornecedor(){
         return $this->belongsTo('App\Models\Fornecedor');
     }
+
+    public function pedidos(){
+        return $this->belongsToMany('App\Models\Pedido', 'pedido_produtos', 'produto_id', 'pedido_id');
+
+        /*
+            3 - Usamos a FK  da tabela mapeada pelo model (produtos), na tabela de relacionamento (pedido_produtos)
+            4 - Representa o nome da FK da tabela mapeada pelo model (pedidos) utilizado no relacionamento que estamos implementando (pedido_produtos)
+        */
+    }
 }
